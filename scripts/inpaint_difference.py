1d540e18de531b4e1746f9da50ba50beb1b4f2ed
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-11-02 19:05:59 -0400

    remove unused script

--- scripts/inpaint_difference.py
+++ scripts/inpaint_difference.py
@@ -1,19 +1,6 @@
-from modules import scripts
-
 from lib_inpaint_difference.globals import DifferenceGlobals
 from lib_inpaint_difference.webui_nasty_hijacks import hijack_gradio_tabs, hijack_img2img_processing
 from lib_inpaint_difference.webui_callbacks import setup_script_callbacks
-
-
-class DummyInpaintDifferenceScript(scripts.Script):
-    def title(self):
-        return 'Inpaint Difference'
-
-    def show(self, is_img2img):
-        return False
-
-    def ui(self, is_img2img):
-        return []
 
 
 if DifferenceGlobals.is_extension_enabled:
