a9fbdbcef9805660cc26284f651ccf06fb114f33
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-11-02 16:23:24 -0400

    remove unused webui context pack manager

--- javascript/ui-layout.js
+++ javascript/ui-layout.js
@@ -17,16 +17,16 @@
     }
 
 
-    function getSwapButton() {
-        return document.querySelector('#img2img_inpaint_difference_swap_images');
-    }
-
-
     function setupCenterStyle() {
         const swapButton = getSwapButton();
         const form = swapButton.parentElement;
         form.style.position = 'relative';
         form.style.flexGrow = '0';
         form.style.padding = '10px';
+    }
+
+
+    function getSwapButton() {
+        return document.querySelector('#img2img_inpaint_difference_swap_images');
     }
 })();
