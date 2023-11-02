b27979bb30bd3118687ea35244e4e70107652571
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-11-01 22:59:02 -0400

    improve image swap event listeners

--- lib_inpaint_difference/ui.py
+++ lib_inpaint_difference/ui.py
@@ -12,9 +12,7 @@ def create_inpaint_difference_tab():
             DifferenceGlobals.inpaint_img_component = gr.Image(label="Base image", source="upload", interactive=True, type="pil", elem_id="img_inpaint_difference")
             swap_images = ToolButton('⇆', elem_id='img2img_inpaint_difference_swap_images', tooltip="Swap images.")
             DifferenceGlobals.inpaint_alt_component = gr.Image(label="Altered image", source="upload", interactive=True, type="pil", elem_id="alt_inpaint_difference")
-
-        with gr.Row():
-            DifferenceGlobals.inpaint_mask_component = gr.Image(label="Difference mask", interactive=False, type="pil", elem_id="mask_inpaint_difference", tool="sketch", height=opts.img2img_editor_height, brush_color=opts.img2img_inpaint_mask_brush_color)
+        DifferenceGlobals.inpaint_mask_component = gr.Image(label="Difference mask", interactive=False, type="pil", elem_id="mask_inpaint_difference", tool="sketch", height=opts.img2img_editor_height, brush_color=opts.img2img_inpaint_mask_brush_color)
 
     def swap_images_func(img, alt):
         DifferenceGlobals.base_image = alt
@@ -41,8 +39,10 @@ def create_inpaint_difference_generation_params_ui():
         'outputs': [DifferenceGlobals.inpaint_mask_component]
     }
 
-    DifferenceGlobals.inpaint_img_component.change(**params)
-    DifferenceGlobals.inpaint_alt_component.change(**params)
+    DifferenceGlobals.inpaint_img_component.upload(**params)
+    DifferenceGlobals.inpaint_img_component.clear(**params)
+    DifferenceGlobals.inpaint_alt_component.upload(**params)
+    DifferenceGlobals.inpaint_alt_component.clear(**params)
     mask_dilation.release(**params)
 
     DifferenceGlobals.ui_params = inpaint_difference_ui_params
