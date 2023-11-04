5b369971cab75922e95d70348876a4dbacd8f032
Plads <34081873+John-WL@users.noreply.github.com>
2023-11-04 12:36:19 -0400

    Update mask_processing.py

--- lib_inpaint_difference/mask_processing.py
+++ lib_inpaint_difference/mask_processing.py
@@ -37,7 +37,7 @@ def compute_mask(
     mask_pil = Image.fromarray(mask.astype(np.uint8), mode=DifferenceGlobals.base_image.mode)
     DifferenceGlobals.generated_mask = mask_pil
 
-    # the rest of the calculations are duplicates of StableDiffusionProcessingImg2Img and/or visual only
+    # the rest of the calculations are either duplicates of StableDiffusionProcessingImg2Img or visual only
     mask = blur(mask, blur_amount)
 
     visual_mask = mask
