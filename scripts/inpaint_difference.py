434849435de5f3a5f4530ed96fc8e70a90994a0b
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-11-07 21:08:19 -0500

    rename file

--- scripts/inpaint_difference.py
+++ scripts/inpaint_difference.py
@@ -4,7 +4,7 @@ plugin = sdlib.register_plugin("inpaint-difference")
 from lib_inpaint_difference.globals import DifferenceGlobals
 from lib_inpaint_difference.webui_nasty_hijacks import hijack_img2img_processing
 from lib_inpaint_difference.webui_callbacks import setup_script_callbacks
-from lib_inpaint_difference.img2img_ui import InpaintDifferenceTab
+from lib_inpaint_difference.ui import InpaintDifferenceTab
 
 
 if DifferenceGlobals.is_extension_enabled:
