bc605694201fd7139273ca903eb1a7050de6ea8b
JohnWL <34081873+John-WL@users.noreply.github.com>
2023-11-22 19:19:46 -0500

    revert temporarily untill sdwss becomes more stable

--- lib_inpaint_difference/webui_callbacks.py
+++ lib_inpaint_difference/webui_callbacks.py
@@ -1,5 +1,6 @@
 from modules.scripts import script_callbacks
 from lib_inpaint_difference.settings import create_settings_section
+from lib_inpaint_difference.img2img_tab_extender import Img2imgTabExtender
 
 
 def on_ui_settings():
@@ -8,3 +9,4 @@ def on_ui_settings():
 
 def setup_script_callbacks():
     script_callbacks.on_ui_settings(on_ui_settings)
+    script_callbacks.on_after_component(Img2imgTabExtender.on_after_component)
