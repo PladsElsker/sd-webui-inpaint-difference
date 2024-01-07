8f0f13f4a32eb88fc7ab2070ff220d488e97c2db
Plads <34081873+John-WL@users.noreply.github.com>
2024-01-07 18:55:10 -0500

    Unified inpaint params (#8) (#9)
    * unified inpaint params (#8)
    
    * rename var (#8)
    
    * rename function (#8)

--- scripts/inpaint_difference.py
+++ scripts/inpaint_difference.py
@@ -1,10 +1,11 @@
 from lib_inpaint_difference.globals import DifferenceGlobals
-from lib_inpaint_difference.webui_nasty_hijacks import hijack_img2img_processing
+from lib_inpaint_difference.webui_nasty_hijacks import hijack_img2img_processing, hijack_ui_settings
 from lib_inpaint_difference.webui_callbacks import setup_script_callbacks
 
 
 if DifferenceGlobals.is_extension_enabled:
     hijack_img2img_processing()
+    hijack_ui_settings()
 
 
 setup_script_callbacks()
