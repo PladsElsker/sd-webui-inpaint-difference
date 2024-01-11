de40ba263b7d8be7639b2396958ed93afa38566e
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-01-10 19:57:19 -0500

    Rename files (#15)

--- scripts/inpaint_difference.py
+++ scripts/inpaint_difference.py
@@ -1,6 +1,6 @@
 from lib_inpaint_difference.globals import DifferenceGlobals
-from lib_inpaint_difference.gradio_hijack import hijack_encode_pil_to_base64
-from lib_inpaint_difference.webui_nasty_hijacks import hijack_img2img_processing, hijack_ui_settings
+from lib_inpaint_difference.gradio_hijacks import hijack_encode_pil_to_base64
+from lib_inpaint_difference.webui_hijacks import hijack_img2img_processing, hijack_ui_settings
 from lib_inpaint_difference.webui_callbacks import setup_script_callbacks
 
 
