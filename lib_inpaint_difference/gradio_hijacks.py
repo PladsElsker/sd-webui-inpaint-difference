15f2bb3179c5d49b0f55b31ddb4322cf70acb766
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-03-13 22:11:37 -0400

    Dimension-agnostic gradio hijack (#19)

--- lib_inpaint_difference/gradio_hijacks.py
+++ lib_inpaint_difference/gradio_hijacks.py
@@ -10,7 +10,10 @@ from lib_inpaint_difference.one_time_callable import one_time_callable
 def hijack_encode_pil_to_base64():
     # from https://github.com/gradio-app/gradio/issues/2635#issuecomment-1423531319
     def encode_pil_to_base64_new(pil_image):
-        image_arr = np.asarray(pil_image)[:, :, ::-1]
+        image_arr = np.asarray(pil_image)
+        if image_arr.ndim == 3:
+            image_arr = np.asarray(pil_image)[:, :, ::-1]
+            
         _, byte_data = imencode('.png', image_arr)
         base64_data = base64.b64encode(byte_data)
         base64_string_opencv = base64_data.decode("utf-8")
