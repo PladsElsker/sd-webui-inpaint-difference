17121fd519532a996dd382fb9fb1534c305693f4
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-03-26 21:36:43 -0400

    hijack sdimg2imgprocessing __init__ instead

--- scripts/inpaint_difference.py
+++ scripts/inpaint_difference.py
@@ -1,12 +1,12 @@
 from lib_inpaint_difference.globals import DifferenceGlobals
 from lib_inpaint_difference.gradio_hijacks import hijack_encode_pil_to_base64
-from lib_inpaint_difference.webui_hijacks import hijack_img2img_processing
+from lib_inpaint_difference.webui_hijacks import hijack_StableDiffusionProcessingImg2Img__init__
 from lib_inpaint_difference.webui_callbacks import setup_script_callbacks
 
 
 if DifferenceGlobals.is_extension_enabled:
     hijack_encode_pil_to_base64()
-    hijack_img2img_processing()
+    hijack_StableDiffusionProcessingImg2Img__init__()
 
 
 setup_script_callbacks(DifferenceGlobals.is_extension_enabled)
