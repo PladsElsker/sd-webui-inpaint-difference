bdcf770e066b75b0d7b0ebf8ad7425a9b908fe60
Plads <lumoximax2@gmail.com>
2024-08-10 08:25:51 -0400

    remove pywin32 and bump sdwi2iextender

--- install.py
+++ install.py
@@ -1,6 +1,5 @@
 import launch
 import pkg_resources
-import platform
 
 
 minimum_sdwi2iextender_version = "0.2.0"
@@ -10,8 +9,3 @@ else:
     current_version = pkg_resources.get_distribution("sdwi2iextender").version
     if pkg_resources.parse_version(current_version) < pkg_resources.parse_version(minimum_sdwi2iextender_version):
         launch.run_pip(f"install sdwi2iextender=={minimum_sdwi2iextender_version}", f"sdwi2iextender=={minimum_sdwi2iextender_version} for sd-webui-inpaint-difference")
-
-
-if platform.system() == "Windows":
-    if not launch.is_installed("pywin32"):
-        launch.run_pip(f"install pywin32", f"pywin32 for sd-webui-inpaint-difference on windows")
